75ae59003ab58226512710fd1328df9d53524289
awololaeinola-lang <awololaeinola@gmail.com>
2026-02-27 23:40:47 -1000

    Fix contact route import path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import contactRoutes from "./src/routes/contact.js"; // default import
+import contactRoutes from "../src/routes/contact.js"; // default import
 
 dotenv.config(); // load env variables
 
